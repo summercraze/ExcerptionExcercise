d9b63c6e3a08a2ac91ffc6032dfa8617581fe0a0
Rong Huang <rhuang92@gmail.com>
2018-02-24 11:46:36 -0800

    final commit
    added comment

--- src/sumMismatch.java
+++ src/sumMismatch.java
@@ -6,17 +6,28 @@ public class sumMismatch
 {
 	public static void main(String[] args)
 	{
+		//open input for user input later
 		Scanner input = new Scanner(System.in);
+		
+		//randomly pick up 2 numbers and figure their sum
 		int integer1 = ThreadLocalRandom.current().nextInt(0, 100 + 1);
 		int integer2 = ThreadLocalRandom.current().nextInt(0, 100 + 1);
 		int sum = integer1 + integer2;
+		
+		//boolean to continue try for number
 		boolean continueInput = true;
+		
+		//while loop to continue ask for sum if the input is wrong
 		do
 		{
+			//try and catch the exception
 			try
 			{
+				//ask for sum input
 				System.out.println("What is the sum of " + integer1 + " and " + integer2 + "?");		
 				int responseSum = input.nextInt();
+				
+				//check whether the sum is correct mathmatically if it is wrong will continue asking
 				if(responseSum != sum)
 				{
 					System.out.println("The answer is not right,try again.");
@@ -34,6 +45,7 @@ public class sumMismatch
 				System.out.println("An integer input is required, try again.");
 				input.nextLine();
 			}
+			
 		}while(continueInput);
 
 	}
